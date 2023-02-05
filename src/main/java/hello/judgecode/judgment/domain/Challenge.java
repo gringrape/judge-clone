864ce991b07f599a25dc75c97a10acdc3f0f2e02
this-is-spear <rjsckdd12@gmail.com>
2023-02-06 01:25:10 +0900

    refact : Challenge 유효성 검사 추가

--- src/main/java/hello/judgecode/judgment/domain/Challenge.java
+++ src/main/java/hello/judgecode/judgment/domain/Challenge.java
@@ -1,6 +1,26 @@
 package hello.judgecode.judgment.domain;
 
+import java.util.Objects;
+
 public record Challenge(Long id, String title, String description, String result) {
+
+  public Challenge {
+    Objects.requireNonNull(title);
+    Objects.requireNonNull(description);
+    Objects.requireNonNull(result);
+
+    if (title.isBlank()) {
+      throw new RuntimeException();
+    }
+
+    if (description.isBlank()) {
+      throw new RuntimeException();
+    }
+
+    if (result.isBlank()) {
+      throw new RuntimeException();
+    }
+  }
 
   public Challenge(String title, String description, String result) {
     this(null, title, description, result);
